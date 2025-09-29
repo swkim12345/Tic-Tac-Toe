a4dc68ff037efaedf74a6e34e3bb9adb00cf2825
yeseong <yeseong31@naver.com>
2025-09-29 16:45:41 +0900

    feat(#2): 게임 완료하기

--- src/Game.jsx
+++ src/Game.jsx
@@ -3,43 +3,76 @@
  * @author yeseong31
  */
 
-// React는 컴포넌트에서 호출하여 무언가를 '기억'할 수 있는 useState()를 제공함
 import { useState } from 'react';
 
-function Square() {
-
-    // value는 값을 저장하고, setValue는 값을 변경하는 데 사용한다.
-    // useState()에 전달된 null은 value의 초깃값으로 사용된다.
-    const [value, setValue] = useState(null);
-
-    function handleClick() {
-        setValue('X');  // value의 값 변경
-    }
-
-    return <button
-        className="square"
-        onClick={handleClick}
-    >
-        {value}
-    </button>;
+function Square({value, onSquareClick}) {
+    return <button className="square" onClick={onSquareClick}>{value}</button>
 }
 
 export default function Board() {
+
+    const [xIsNext, setXIsNext] = useState(true);
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    function handleClick(i) {
+        if (squares[i] || calculateWinner(squares)) return;
+
+        const nextSquares = squares.slice();
+        if (xIsNext) {
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
+        setSquares(nextSquares);
+        setXIsNext(!xIsNext);
+    }
+
+    function calculateWinner(squares) {
+        const lines  = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
+    const winner = calculateWinner(squares);
+    let status;
+
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next Player: " + (xIsNext ? 'X' : 'O');
+    }
+
     return <>
+        <div className="status">{status}</div>
         <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
+            <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+            <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+            <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
         </div>
         <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
         </div>
         <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
         </div>
     </>
 }
