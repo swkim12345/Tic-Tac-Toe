5ccf1cd6e03e85e931353e03949b465d8a7058fb
yeseong <yeseong31@naver.com>
2025-09-29 15:51:50 +0900

    feat(#1): 개요 및 코드 살펴보기

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,10 +1,45 @@
 /**
  * Tic Tac Toe
- * @author (your name)
+ * @author yeseong31
  */
 
-export default function Game() {
-  return <>
-    <h1>Tic Tac Toe</h1>
-  </>
+// React는 컴포넌트에서 호출하여 무언가를 '기억'할 수 있는 useState()를 제공함
+import { useState } from 'react';
+
+function Square() {
+
+    // value는 값을 저장하고, setValue는 값을 변경하는 데 사용한다.
+    // useState()에 전달된 null은 value의 초깃값으로 사용된다.
+    const [value, setValue] = useState(null);
+
+    function handleClick() {
+        setValue('X');  // value의 값 변경
+    }
+
+    return <button
+        className="square"
+        onClick={handleClick}
+    >
+        {value}
+    </button>;
+}
+
+export default function Board() {
+    return <>
+        <div className="board-row">
+            <Square />
+            <Square />
+            <Square />
+        </div>
+        <div className="board-row">
+            <Square />
+            <Square />
+            <Square />
+        </div>
+        <div className="board-row">
+            <Square />
+            <Square />
+            <Square />
+        </div>
+    </>
 }
