44e7b6d11b4532e69b0faa9a290ad26cc666766c
junyonglee0223 <ljyong3339@gmail.com>
2025-09-30 17:51:51 +0900

    feat: 승자 판별 및 상태 출력 calculateWinner 구현 승자 판별 로직 추가 및 현 플레이서 상태 출력, 승리자 출력 기능 추가

--- src/Game.jsx
+++ src/Game.jsx
@@ -15,10 +15,17 @@ function Square({ value, onSquareClick }) {
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "O");
+  }
 
   function handleClick(i) {
     const nextSquares = squares.slice();
-    if (nextSquares[i] != null) return;
+    if (nextSquares[i] != null || calculateWinner(nextSquares)) return;
     if (xIsNext) nextSquares[i] = "X";
     else nextSquares[i] = "O";
     setXIsNext(!xIsNext);
@@ -26,6 +33,7 @@ export default function Board() {
   }
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -43,4 +51,24 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
