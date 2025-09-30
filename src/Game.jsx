f41f41dda152a1bda8362263ae330983a2bb6e1f
seunghun <kangseunghun9927@gmail.com>
2025-09-30 20:44:30 +0900

    작업 중

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,10 +1,83 @@
 /**
  * Tic Tac Toe
- * @author (your name)
+ * @author (SeungHun)
  */
 
-export default function Game() {
-  return <>
-    <h1>Tic Tac Toe</h1>
-  </>
+import { useState } from "react";
+
+// export default function Board() {
+//   return (
+//     <>
+//       <div>
+//         <button className="square">1</button>
+//         <button className="square">2</button>
+//         <button className="square">3</button>
+//       </div>
+//       <div>
+//         <button className="square">4</button>
+//         <button className="square">5</button>
+//         <button className="square">6</button>
+//       </div>
+//       <div>
+//         <button className="square">7</button>
+//         <button className="square">8</button>
+//         <button className="square">9</button>
+//       </div>
+//     </>
+//   );
+// }
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
+
+export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
+
+// export default function Game() {
+//   return (
+//     <>
+//       <h1>Tic Tac Toe</h1>
+//     </>
+//   );
+// }
