52da7f9d3a0d56edf67bf284b62989d542e85fda
junyonglee0223 <ljyong3339@gmail.com>
2025-09-30 16:51:35 +0900

    feat: create basic board filling with 1 to 9 board function 내부에 button 속성을 만들어서 사용 props로 데이터 전달

--- src/Game.jsx
+++ src/Game.jsx
@@ -3,8 +3,29 @@
  * @author (your name)
  */
 
-export default function Game() {
-  return <>
-    <h1>Tic Tac Toe</h1>
-  </>
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
+export default function Board() {
+  // return <button className="square">X</button>;
+  return (
+    <>
+      <div className="board-row">
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
+      </div>
+      <div className="board-row">
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
+      </div>
+      <div className="board-row">
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
+      </div>
+    </>
+  );
 }
